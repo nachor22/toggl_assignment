57b8ef42427ef2f03a88cd87c72a1d2c7fd1d602
Nacho Retamoza <nachor22@gmail.com>
2021-06-20 20:26:31 -0300

    add comments

--- main.tf
+++ main.tf
@@ -3,15 +3,23 @@ variable "project" {
 }
 
 variable "region" {
+  description = "GCP region to be used"
   default = "us-central1"
 }
 
 variable "zone" {
+  description = "GCP zone to be used"
   default = "us-central1-a"
 }
 
 variable "zone_consul" {
+  description = "GCP zone to deploy consul cluster"
   default = "us-east1-b"
+}
+
+variable "dns_zone" {
+  description = "DNS zone to manage"
+  default = "nachor22.tk"
 }
 
 provider "google" {
