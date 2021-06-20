161fe662d1ffcb8a04c899dbfa67830b64388e1a
Nacho Retamoza <nachor22@gmail.com>
2021-06-20 19:53:12 -0300

    clean code

--- 01_bucket.tf
+++ 01_bucket.tf
@@ -1,25 +1,23 @@
-resource "google_storage_bucket" "toggl_test_static_content" {
-  name        = "toggl_test_static_content"
+resource "google_storage_bucket" "public" {
+  name        = "toggl-public"
+}
+
+resource "google_storage_default_object_access_control" "public_rule" {
+  bucket = google_storage_bucket.public.name
+  role   = "READER"
+  entity = "allUsers"
 }
 
 resource "google_storage_bucket_object" "index" {
   name   = "index.html"
   source = "public/index.html"
-  bucket = google_storage_bucket.toggl_test_static_content.name
+  bucket = google_storage_bucket.public.name
+  depends_on = [google_storage_default_object_access_control.public_rule]
 }
 
 resource "google_storage_bucket_object" "main" {
   name   = "main.js"
   source = "public/main.js"
-  bucket = google_storage_bucket.toggl_test_static_content.name
-}
-
-resource "google_storage_bucket_access_control" "public_rule" {
-  bucket = google_storage_bucket.toggl_test_static_content.name
-  role   = "READER"
-  entity = "allUsers"
-}
-
-output "bucket_domain" {
-  value = google_storage_bucket_object.index.media_link
+  bucket = google_storage_bucket.public.name
+  depends_on = [google_storage_default_object_access_control.public_rule]
 }
